4ef975bee49f4f9a7401f4530a635bda366de4ee
boss hoss <judy.combs.reyes@gmail.com>
2019-11-25 16:21:38 -0800

    11/25/19

--- charbucket/charbucket/urls.py
+++ charbucket/charbucket/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('charapp/', include('charapp.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
